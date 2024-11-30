b0402a12a1968dc1ed6b351d49019f8ddb9dc924
Javid Momeni <j.momeni@parsa.ir>
2024-11-30 14:35:58 +0330

    Fix: changes to fix deploying on github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  trailingSlash: true,
+  output: 'export',
   experimental: {
     optimizePackageImports: ['@mantine/core', '@mantine/hooks'],
   },
